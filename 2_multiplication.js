d2a3df418a5224404255b181161471ba855d5d89
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-18 01:43:10 +0700

    multiplication function with nested loop

---
+++ 2_multiplication.js
@@ -0,0 +1,11 @@
+const getMultiplicationTable = (num) => {
+  for (let i = 1; i <= num; i++) {
+    let baris = ""
+    for (let j = 1; j <= num; j++) {
+      baris+= i * j
+    }
+    console.log(baris)
+  }
+}
+
+getMultiplicationTable(5)
