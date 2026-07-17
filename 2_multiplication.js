bb37b75bd301311a02cdc8e772b31d0a161b8836
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-18 01:48:58 +0700

    Improve formatting of multiplication table output
    Added tab spacing between multiplication results for better formatting.

--- 2_multiplication.js
+++ 2_multiplication.js
@@ -2,7 +2,7 @@ const getMultiplicationTable = (num) => {
   for (let i = 1; i <= num; i++) {
     let baris = ""
     for (let j = 1; j <= num; j++) {
-      baris+= i * j
+      baris+= i * j + '\t'
     }
     console.log(baris)
   }
